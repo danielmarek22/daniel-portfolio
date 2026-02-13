95b97e50d7e3ffb7215c61ccfbe816d4121edd1c
dmarek <bsh@pop-os.localdomain>
2026-02-13 20:08:43 +0100

    Fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
 
-const isProd = process.env.NODE_ENV === "production";
-
 export default defineConfig({
   site: "https://danielmarek22.github.io",
-  base: isProd ? "/daniel-portfolio/" : "/",
+  base: "/daniel-portfolio/",
   output: "static",
 
   vite: {
