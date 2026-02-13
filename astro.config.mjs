110f19027f5579ec2115377d24d9cfe890f08bd5
dmarek <bsh@pop-os.localdomain>
2026-02-13 19:57:51 +0100

    Upload v1 of the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   site: "https://danielmarek22.github.io",
-  base: "/daniel-portfolio/",
+  base: isProd ? "/daniel-portfolio/" : "/",
   output: "static",
 
   vite: {
