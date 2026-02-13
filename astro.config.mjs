125c679daa89d5e95c7dd3026b4d572ca52c55ba
dmarek <bsh@pop-os.localdomain>
2026-02-13 13:50:25 +0100

    Add github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  site: "https://danielmarek22.github.io",
+  base: "/daniel-portfolio/",
+  output: "static",
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
